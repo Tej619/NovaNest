1f45f3c237682be56aa22fa17464a33112514695
Tejas Vaity <tejas.vaity3@gmail.com>
2025-09-07 00:31:20 -0400

    added the routes in index.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import { authMiddleware } from "./middleware/authMiddleware";
+import tenantRoutes from "./routes/tenantRoutes";
+import managerRoutes from "./routes/managerRoutes";
+import propertyRoutes from "./routes/propertyRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 dotenv.config();
 const app = express();
@@ -18,9 +24,14 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
+app.use("/applications", applicationRoutes);
+app.use("/properties", propertyRoutes);
+app.use("/leases", leaseRoutes);
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
+app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
 
 const port = Number(process.env.PORT) || 3002;
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
