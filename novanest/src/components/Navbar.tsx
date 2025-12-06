e32754d3b6e727279c04310e26bdcc039b1d839b
Tejas Vaity <tejas.vaity3@gmail.com>
2025-12-05 20:49:08 -0500

    removing logo

--- novanest/src/components/Navbar.tsx
+++ novanest/src/components/Navbar.tsx
@@ -50,13 +50,6 @@ const Navbar = () => {
             scroll={false}
           >
             <div className="flex items-center gap-3">
-              {/* <Image
-                src="/logo.svg"
-                alt="Rentiful Logo"
-                width={24}
-                height={24}
-                className="w-6 h-6"
-              /> */}
               <div className="text-xl font-bold">
                 NOVA
                 <span className="text-secondary-500 font-light hover:!text-primary-300">
